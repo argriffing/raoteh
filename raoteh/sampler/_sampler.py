6c1cfe583026bbb0f53562706f0235139fb978e5
alex <argriffi@ncsu.edu>
2013-05-17 14:46:26 -0400

    ENH: add a kind of dual tree construction

--- raoteh/sampler/_sampler.py
+++ raoteh/sampler/_sampler.py
@@ -4,6 +4,8 @@ Rao-Teh sampler.
 """
 from __future__ import division, print_function, absolute_import
 
+from collections import defaultdict
+
 import networkx as nx
 
 __all__ = ['mysum']
@@ -11,6 +13,10 @@ __all__ = ['mysum']
 # This is for checking the plumbing.
 def mysum(*args):
     return sum(args)
+
+def get_first_element(elements):
+    for x in elements:
+        return x
 
 def get_edge_bisected_graph(G):
     """
@@ -45,6 +51,97 @@ def get_edge_bisected_graph(G):
         G_out.add_edge(a, mid, weight=half_weight)
         G_out.add_edge(mid, b, weight=half_weight)
     return G_out
+
+
+def get_chunk_tree(T, event_nodes, root=None):
+    """
+    Construct a certain kind of dual graph.
+
+    Parameters
+    ----------
+    T : undirected acyclic networkx graph
+        Input tree graph with integer nodes.
+    event_nodes : set of integers
+        This subset of nodes in the input graph
+        will have bijective correspondence to edges in the output graph.
+
+    Returns
+    -------
+    chunk_tree : undirected unweighted acyclic networkx graph
+        A kind of dual tree with new nodes and edges.
+    non_event_node_map : dict
+        A map from each non-event node in the original graph
+        to a node in the output graph.
+    event_node_map : dict
+        A map from each event node in the original graph
+        to a pair of adjacent nodes in the output graph.
+
+    Notes
+    -----
+    Edges in the output chunk graph correspond bijectively to event nodes
+    in the original graph.
+
+    """
+
+    # Partition the input nodes into event and non-event nodes.
+    non_event_nodes = set(T) - set(event_nodes)
+
+    # Initialize the outputs.
+    chunk_tree = nx.Graph()
+    non_event_node_map = {}
+    event_node_map = {}
+
+    # Populate the outputs,
+    # traversing the input graph in preorder from an arbitrary non-event root.
+    next_chunk = 0
+    if root is None:
+        root = get_first_element(non_event_nodes)
+    non_event_node_map[root] = next_chunk
+    chunk_tree.add_node(next_chunk)
+    next_chunk += 1
+    for a, b in nx.bfs_edges(T, root):
+
+        # Get the chunk associated with the parent node.
+        if a in non_event_nodes:
+            parent_chunk = non_event_node_map[a]
+        elif a in event_nodes:
+            grandparent_chunk, parent_chunk = event_node_map[a]
+        else:
+            raise Exception('internal error')
+
+        # If the child node is an event node,
+        # then begin a new chunk and add an edge to the output graph.
+        # Otherwise update the output maps but do not modify the output graph.
+        if b in event_nodes:
+            chunk_edge = (parent_chunk, next_chunk)
+            event_node_map[b] = chunk_edge
+            chunk_tree.add_edge(*chunk_edge)
+            next_chunk += 1
+        elif b in non_event_nodes:
+            non_event_node_map[b] = parent_chunk
+        else:
+            raise Exception('internal error')
+
+    # Return the outputs.
+    return chunk_tree, non_event_node_map, event_node_map
+
+
+def resample_states(T, tip_to_state, P):
+    """
+
+    Parameters
+    ----------
+    T : weighted directed networkx tree graph
+        Defines the tree shape.
+    tip_to_state : dict
+        Maps tip nodes to states.
+
+    Notes
+    -----
+    Input edge weights and states are disregarded.
+
+    """
+    pass
 
 
 def get_feasible_history(rate_matrix, tip_states, tree):
--- raoteh/sampler/tests/test_sampler.py
+++ raoteh/sampler/tests/test_sampler.py
@@ -42,6 +42,59 @@ class TestSampler(TestCase):
         # The added nodes are each greater than each original node.
         assert_(max(G) < min(set(H) - set(G)))
 
+    def test_get_chunk_tree(self):
+
+        # Define the original tree and its event nodes.
+        # This is taken from a doodle in my notebook,
+        # and it is not particularly cleverly chosen.
+        tree_edges = (
+                (0, 1),
+                (1, 2),
+                (3, 4),
+                (4, 2),
+                (2, 5),
+                (5, 6),
+                (6, 7),
+                (7, 8),
+                (8, 9),
+                (7, 10),
+                (10, 11),
+                (11, 12),
+                (12, 13),
+                (13, 14),
+                (13, 15),
+                (15, 16),
+                (16, 17),
+                )
+        event_nodes = {1, 4, 5, 6, 8, 10, 11, 12, 15, 16}
+
+        # Create a tree by specifying the edges.
+        T = nx.Graph()
+        T.add_edges_from(tree_edges)
+
+        # Run tests, using all possible roots and also a default root.
+        potential_roots = list(T) + [None]
+        for root in potential_roots:
+
+            # Construct the chunk tree and its associated node maps.
+            chunk_tree, non_event_map, event_map = _sampler.get_chunk_tree(
+                    T, event_nodes)
+            
+            # The nodes pointed to by the non_event_map
+            # should be nodes in the chunk_tree.
+            assert_(set(non_event_map.values()) <= set(T))
+
+            # The output tree should have 11 nodes and 10 edges.
+            assert_equal(len(chunk_tree), 11)
+            assert_equal(len(chunk_tree.edges()), 10)
+
+            # The 8 non-event nodes should map to 7 unique chunk nodes.
+            assert_equal(len(non_event_map), 8)
+            assert_equal(len(set(non_event_map.values())), 7)
+
+            # The non-event nodes 13 and 14 should map to the same chunk.
+            assert_equal(non_event_map[13], non_event_map[14])
+
 
 if __name__ == '__main__':
     run_module_suite()
