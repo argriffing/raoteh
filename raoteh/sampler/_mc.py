e05947b516b112ea0295ae34fda7ba84c52e5ad2
alex <argriffi@ncsu.edu>
2013-06-10 12:27:55 -0400

    MAINT: redirect _mc likelihood to _mcy likelihood

--- raoteh/sampler/_mc.py
+++ raoteh/sampler/_mc.py
@@ -39,53 +39,9 @@ def construct_node_to_restricted_pmap(
             P_default=P_default)
 
 
+#TODO under destruction
 def get_restricted_likelihood(T, root, node_to_allowed_states,
         root_distn=None, P_default=None):
-    """
-    Compute a likelihood or raise an exception if the likelihood is zero.
-
-    This is a general likelihood calculator for
-    a Markov chain on tree-structured domains.
-    At each node in the tree, the set of possible states may be restricted.
-    Lack of state restriction at a node corresponds to missing data;
-    a common example of such missing data would be missing states
-    at internal nodes in a tree.
-    Alternatively, a node could have a completely specified state,
-    as could be the case if the state of the process is completely
-    known at the tips of the tree.
-    More generally, a node could be restricted to an arbitrary set of states.
-    The first three args are used to construct a map from each node
-    to a map from the state to the subtree likelihood,
-    and the last arg defines the initial conditions at the root.
-
-    Parameters
-    ----------
-    T : undirected acyclic networkx graph
-        A tree whose edges are annotated with transition matrices P.
-    root : integer
-        The root node.
-    node_to_allowed_states : dict
-        A map from a node to a set of allowed states.
-    root_distn : dict, optional
-        A sparse finite distribution or weights over root states.
-        Values should be positive but are not required to sum to 1.
-        If the distribution is not provided,
-        then it will be assumed to have values of 1 for each possible state.
-    P_default : directed weighted networkx graph, optional
-        If an edge is not annotated with a transition matrix P,
-        then this default transition matrix will be used.
-
-    Returns
-    -------
-    likelihood : float
-        The likelihood.
-
-    """
-    # Get likelihoods conditional on the root state.
-    node_to_pmap = construct_node_to_restricted_pmap(
-            T, root, node_to_allowed_states, P_default=P_default)
-    root_pmap = node_to_pmap[root]
-
-    # Return the likelihood.
-    return _mc0.get_likelihood(root_pmap, root_distn=root_distn)
+    return _mcy.get_likelihood(T, root, node_to_allowed_states,
+        root_distn=root_distn, P_default=P_default)
 
