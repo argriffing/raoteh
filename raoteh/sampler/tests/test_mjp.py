dcadccb433ae190a9ab8bfe68969782497cc65dd
alex <argriffi@ncsu.edu>
2013-07-01 09:15:53 -0400

    TST: likelihood tests for _mjp and _mjp_dense

--- raoteh/sampler/tests/test_mjp.py
+++ raoteh/sampler/tests/test_mjp.py
@@ -10,7 +10,7 @@ import networkx as nx
 from numpy.testing import (run_module_suite, TestCase,
         assert_equal, assert_allclose, assert_)
 
-from raoteh.sampler import _mjp, _mjp_dense
+from raoteh.sampler import _density, _mjp, _mjp_dense
 
 from raoteh.sampler._density import (
         dict_to_numpy_array,
@@ -47,6 +47,81 @@ class TestMarkovJumpProcess(TestCase):
         desired_dense = dict_to_numpy_array(expected, nodelist=nodelist)
         actual_dense = _mjp_dense.get_total_rates(Q_dense)
         assert_allclose(actual_dense, desired_dense)
+
+    def test_get_likelihood(self):
+        T = nx.Graph()
+        T.add_weighted_edges_from([
+            (0, 1, 2.0),
+            (0, 2, 3.0),
+            (0, 3, 4.0),
+            (1, 4, 5.0),
+            (1, 5, 6.0),
+            ])
+        nstates = 4
+        distn = {
+                0 : 0.1,
+                1 : 0.2,
+                2 : 0.3,
+                3 : 0.4,
+                }
+        Q = nx.DiGraph()
+        Q.add_weighted_edges_from([
+            (0, 1, 1.0 * distn[1]),
+            (1, 0, 1.0 * distn[0]),
+            (1, 2, 2.0 * distn[2]),
+            (2, 1, 2.0 * distn[1]),
+            (2, 3, 1.0 * distn[3]),
+            (3, 2, 1.0 * distn[2]),
+            (3, 0, 2.0 * distn[0]),
+            (0, 3, 2.0 * distn[3]),
+            ])
+        node_to_allowed_states = {
+                0 : set(range(nstates)),
+                1 : set(range(nstates)),
+                2 : {0},
+                3 : {1},
+                4 : {2},
+                5 : {3},
+                }
+        distn_dense = _density.dict_to_numpy_array(
+                distn, nodelist=range(nstates))
+        Q_dense = _density.rate_matrix_to_numpy_array(
+                Q, nodelist=range(nstates))
+        for root in range(6):
+            lk = _mjp.get_likelihood(
+                    T, node_to_allowed_states, root,
+                    root_distn=distn, Q_default=Q)
+            lk_dense = _mjp_dense.get_likelihood(
+                    T, node_to_allowed_states, root, nstates,
+                    root_distn=distn_dense, Q_default=Q_dense)
+            assert_allclose(lk, lk_dense)
+        lk_marginal = lk
+        lk_dense_marginal = lk_dense
+
+        # Compute the likelihood by naively summing over all combinations
+        # of ancestral states.
+        lk_m = 0
+        lk_dense_m = 0
+        for s0 in range(4):
+            for s1 in range(4):
+                root = 0
+                nodemap = dict(node_to_allowed_states)
+                nodemap[0] = {s0}
+                nodemap[1] = {s1}
+                for root in range(6):
+                    lk = _mjp.get_likelihood(
+                            T, nodemap, root,
+                            root_distn=distn, Q_default=Q)
+                    lk_dense = _mjp_dense.get_likelihood(
+                            T, nodemap, root, nstates,
+                            root_distn=distn_dense, Q_default=Q_dense)
+                    assert_allclose(lk, lk_dense)
+                lk_m += lk
+                lk_dense_m += lk_dense
+
+        # Check that the answers are the same.
+        assert_allclose(lk_marginal, lk_m)
+        assert_allclose(lk_dense_marginal, lk_dense_m)
 
     def test_jukes_cantor_conditional_expectation(self):
         # Compare conditional expectations to the true values.
