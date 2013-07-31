3b3ae29cb4ce4b3fbcd78a605954236e3c7cbdea
alex <argriffi@ncsu.edu>
2013-07-31 09:45:49 -0400

    DOC: add C source comments

--- craoteh/sampler.c
+++ craoteh/sampler.c
@@ -8,8 +8,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <gsl/gsl_rng.h>
 
 int main(int argc, char **argv)
+{
+  /* Read the options, or rather use a control file.
+   * - tree, using python csr format;
+   *   http://docs.scipy.org/doc/scipy/reference/generated/
+   *   scipy.sparse.csr_matrix.html; several arrays in the same file
+   *   - indptr (1d array of indices -- pointers into an array)
+   *   - indices (1d array of indices -- child node indices)
+   *   - data (1d array of indices -- branch lengths)
+   *   - preorder (1d array of indices -- first index corresponds to the root)
+   * - tolerance process parameters
+   *   - rate off to on (a floating point number)
+   *   - rate on to off (a floating point number)
+   * - primary process parameters
+   *   - primary process rate matrix (2d array of floating point numbers)
+   *   - prior distribution at the root (1d array of floating point numbers)
+   * - connection between the primary process and the tolerance processes
+   *   - map from primary state to tolerance class
+   * - data; for every site in the alignment, for every node in the tree
+   *   - set of allowed primary states
+   *   - for each tolerance class, the set of allowed tolerance states
+   */
+
+  const gsl_rng_type * T;
+
+  return 0;
+}
+
+
+int foo()
 {
   int aflag = 0;
   int bflag = 0;
