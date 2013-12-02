589ea560afbb3946e72802cf5eb09fda882652e4
alex <argriffi@ncsu.edu>
2013-12-02 00:44:05 -0500

    add more code to the example

--- examples/p53/liwen.py
+++ examples/p53/liwen.py
@@ -258,6 +258,40 @@ def get_jeff_params_d():
     return (kappa, omega, A, C, T, G, rho,
             tree, root, leaf_name_pairs)
 
+def get_jeff_params_e():
+    """
+    Added early December 2013 in response to email from Jeff.
+    Use these to compute a log likelihood and per-branch
+    expected switching counts (equivalently probabilities because
+    at most one switch is allowed per branch).
+    The log likelihood should be summed over all p53 codon sites
+    and the expected switching counts should be averaged
+    over all codon sites.
+    """
+
+    # FINAL ESTIMATE: rho12 =    0.61610
+    # FINAL ESTIMATE: for frequency of purines is    0.50862
+    # FINAL ESTIMATE: for freq. of A among purines is    0.49373
+    # FINAL ESTIMATE: for freq. of T among pyrimidines is    0.38884
+    # FINAL ESTIMATE: kappa =    3.38714
+    # FINAL ESTIMATE: omega =    0.37767
+
+    rho = 0.61610
+    AG = 0.50862
+    CT = 1 - AG
+    A = AG * 0.49373
+    G = AG - A
+    T = CT * 0.38884
+    C = CT - T
+    kappa = 3.38714
+    omega = 0.37767
+
+    tree_string = """((((((Has:  0.0073385245,Ptr:  0.0073385245):  0.0640509640,Ppy:  0.0713894884):  0.0542000118,(((Mmu:  0.0025462071,Mfu:  0.0025462071):  0.0000000000,Mfa:  0.0025462071):  0.0318638454,Cae:  0.0344100525):  0.0911794477):  0.1983006745,(Mim:  0.3238901743,Tgl:  0.3238901743):  0.0000000004):  0.2277808059,((((((Mum:  0.1797319785,Rno:  0.1797319785):  0.1566592047,Mun:  0.3363911832):  0.0192333544,(Cgr:  0.1074213106,Mau:  0.1074213106):  0.2482032271):  0.0447054051,Sju:  0.4003299428):  0.1000000288,(Cpo:  0.4170856630,Mmo:  0.4170856630):  0.0832443086):  0.0250358682,(Ocu:  0.4149196099,Opr:  0.4149196099):  0.1104462299):  0.0263051408):  0.0000000147,(Sar:  0.4524627987,((Fca:  0.2801000848,Cfa:  0.2801000848):  0.1338023902,((Bta:  0.0880000138,Oar:  0.0880000138):  0.1543496707,Dle:  0.2423496845):  0.1715527905):  0.0385603236):  0.0992081966);"""
+    fin = StringIO(tree_string)
+    tree, root, leaf_name_pairs = app_helper.read_newick(fin)
+    return (kappa, omega, A, C, T, G, rho,
+            tree, root, leaf_name_pairs)
+
 
 def get_codeml_estimated_params():
 
@@ -398,7 +432,8 @@ def main(args):
     #info = get_jeff_params()
     #info = get_jeff_params_b()
     #info = get_jeff_params_c()
-    info = get_jeff_params_d()
+    #info = get_jeff_params_d()
+    info = get_jeff_params_e()
     kappa, omega, A, C, T, G, rho, tree, root, leaf_name_pairs = info
     name_to_leaf = dict((name, leaf) for leaf, name in leaf_name_pairs)
 
@@ -603,9 +638,6 @@ def main(args):
         # Define the t -> P callbacks for the true process
         # or for a time-discretized process, depending on cmdline flags.
 
-        if args.dt is None:
-            raise argparse.ArgumentError('for now, require dt...')
-
         # Define an SD decomposition of the reference process.
         D0 = reference_distn_dense
         S0 = qtop.dot_square_diag(Q_reference_dense, qtop.pseudo_reciprocal(D0))
@@ -730,6 +762,7 @@ if __name__ == '__main__':
     parser.add_argument('--disease', required=True,
             help='csv file with filtered disease data')
     parser.add_argument('--dt', type=float,
+            required=True, # for now require dt
             help='discretize the tree with this maximum branchlet length')
     parser.add_argument('--lb', action='store_true',
             help='compute a lower bound instead of an approximation')
