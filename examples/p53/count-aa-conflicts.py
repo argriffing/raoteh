b6ab69aee917239c889d4c939b5925d470389b44
alex <argriffi@ncsu.edu>
2013-12-13 22:39:36 -0500

    add leaf background color and finally figure out tspan

--- examples/p53/count-aa-conflicts.py
+++ examples/p53/count-aa-conflicts.py
@@ -4,11 +4,15 @@ For each p53 amino acid position, compare disease amino acids to wild types.
 For each position in the p53 codon alignment,
 count the number of taxa whose wild-type amino acid at that position
 has been found in a human tumor.
+
+Per-leaf information is also written per site.
+
 Write the output in json format for javascript visualization compatibility.
 
 """
 from __future__ import print_function, division
 
+import argparse
 from collections import defaultdict
 import json
 
@@ -64,23 +68,26 @@ def get_disease_info(fin):
     return pos_set, pos_to_lethal_residues
 
 
-def main():
+def main(args):
 
     # Read the interpreted disease data.
     # It returns a map from codon position to lethal residue set.
-    print('reading interpreted disease data...')
+    if args.verbose:
+        print('reading interpreted disease data...')
     with open(g_interpreted_disease_filename) as fin:
         pos_set, pos_to_lethal_residues = get_disease_info(fin)
 
     # Read the codon alignment.
     # It is a list of (taxon_name, codon_list) pairs.
-    print('reading codon alignment...')
+    if args.verbose:
+        print('reading codon alignment...')
     with open(g_alignment_filename) as fin:
         name_codons_list = list(app_helper.read_phylip(fin))
 
     # Read the genetic code.
     # It is a list of (state, residue, codon) triples.
-    print('reading genetic code...')
+    if args.verbose:
+        print('reading genetic code...')
     with open(g_genetic_code_filename) as fin:
         genetic_code = app_helper.read_genetic_code(fin)
     codon_to_residue = dict((c, r) for s, r, c in genetic_code)
@@ -91,18 +98,34 @@ def main():
         codon_pos = i + 1
         nconflicts = 0
         lethal_residues = pos_to_lethal_residues.get(codon_pos, None)
-        if lethal_residues is None:
-            # No variation was found in human tumors at this site.
-            pass
-        else:
-            for name, codons in name_codons_list:
-                residue = codon_to_residue[codons[i]]
-                if residue in lethal_residues:
-                    nconflicts += 1
-        json_dicts.append({'pos' : codon_pos, 'nconflicts' : nconflicts})
+
+        # Get the leaf info and count the number of wild-type disease residues.
+        leaf_info = []
+        for idx, (name, codons) in enumerate(name_codons_list):
+            codon = codons[i]
+            residue = codon_to_residue[codon]
+            disease = 0
+            if lethal_residues and residue in lethal_residues:
+                disease = 1
+            leaf_info.append({
+                'idx' : idx,
+                'name' : name,
+                'codon' : codon,
+                'residue' : residue,
+                'disease' : disease,
+                })
+            nconflicts += disease
+
+        json_dicts.append({
+            'pos' : codon_pos,
+            'nconflicts' : nconflicts,
+            'leaf_info' : leaf_info,
+            })
 
     # Write the json data.
     print(json.dumps(json_dicts, indent=2))
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', action='store_true')
+    main(parser.parse_args())
